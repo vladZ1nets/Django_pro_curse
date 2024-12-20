f3f8c0c9420f13ab02e6dba2204770ae42a343c7
vladZ1nets <vlad.zinets@gmail.com>
2024-12-20 13:47:12 +0200

    up

--- trainer/urls.py
+++ trainer/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<category_id>", views.category_page, name="category_page"),
     path("<trainer_id>", views.trainer_page, name="trainer_page"),
-    path("<trainer_id>/<service_id>", views.service_page, name="service_page"),
+    path("<trainer_id>/<service_id>", views.trainer_service_page, name="train_service_page"),
     path("<trainer_id>/<service_id>/booking", views.booking_for_user, name="booking_for_user"),
 ]
--- trainer/views.py
+++ trainer/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import trainer.models
+
 # Create your views here.
 def category_page(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def trainer_page(request, user_id):
+def trainer_page(request, trainer_id):
+    if request.method == "GET":
+        service_categories = trainer.models.Category.objects.all()
+
+        my_services = trainer.models.Service.objects.filter(trainer=request.user).all()
+
+        return render(request,"trainer.html", {"categories": service_categories, "services": my_services}) # форма з додаванням сервісу
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def trainer_service_page(request, user_id, service_id):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def service_page(request):
+    if request.method == "POST":
+        form_data = request.POST
+        service_cat = trainer.models.Category.objects.get(pk=form_data["category"])
+        service = trainer.models.Service(
+          level = form_data["level"],
+          duration = form_data["duration"],
+          price = form_data["price"],
+          category = service_cat,
+          trainer = request.user,
+        )
+        service.save()
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def booking_for_user(request):
