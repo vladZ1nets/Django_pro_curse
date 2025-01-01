019fe8a4df1dd26a348e0ed94fcf0494466610f0
vladZ1nets <vlad.zinets@gmail.com>
2025-01-01 11:26:53 +0200

    Update urls.py

--- TrainerApp/urls.py
+++ TrainerApp/urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for TrainerApp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 import users.views
 import trainer.views
 import booking.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("users.urls")),
@@ -32,7 +17,4 @@ urlpatterns = [
     path("service/", trainer.views.service_page, name="trainer_service"),
     path("userbook/", trainer.views.booking_for_user, name="booking_for_user"),
     path("booking/", include("booking.urls")),
-    path("cancel/", booking.views.booking_cancel, name="booking_cancel"),
-    path("acception/", booking.views.booking_acception, name="booking_acception"),
-
 ]
