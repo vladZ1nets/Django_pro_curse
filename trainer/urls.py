6854e3320116425f010ec0f77d55477d2d0bb451
vladZ1nets <vlad.zinets@gmail.com>
2025-01-01 11:29:00 +0200

    Update urls.py

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("<trainer_id>", views.trainer_page, name="trainer_page"),
-    path("<trainer_id>/<service_id>", views.trainer_service_page, name="train_service_page"),
-    path("<trainer_id>/<service_id>/booking", views.booking_for_user, name="booking_for_user"),
+    path("categories/", views.category_page, name="category_page"),
+    path("<int:trainer_id>/", views.trainer_page, name="trainer_page"),
+    path("<int:trainer_id>/<int:service_id>/", views.trainer_service_page, name="trainer_service_page"),
+    path("<int:trainer_id>/<int:service_id>/booking/", views.booking_for_user, name="booking_for_user"),
+    path("register/", views.trainer_registration, name="trainer_registration"),
+    path("services/", views.service_page, name="service_page"),
 ]
